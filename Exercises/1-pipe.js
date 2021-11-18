93869ae3d3ae41015cd22367ef527ab298295abe
Grevcev.AS <grevcev.as@dns-shop.ru>
2021-11-18 18:24:28 +1000

    compose lab

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => {
+  if (fns.some(f => 'function' !== typeof(f))) {
+    throw new Error('All arguments must be functions');
+  }
+
+  return x => fns.reduce((v, c) => c(v), x);
+};
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,24 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const compose = (...fns) => {
+  const callbacks = [];
+
+  const func = x => {
+    let res = x;
+    for (const f of fns.reverse()) {
+      try {
+        res = f(res);
+      } catch (err) {
+        callbacks.forEach(cb => cb());
+        return;
+      }
+    }
+    return res;
+  };
+
+  func.on = (str, fn) => ('error' === str) && callbacks.push(fn);
+
+  return func;
+};
 
 module.exports = { compose };
